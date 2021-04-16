391ea937c25b43d505dae829cd99419f05d60217
PINAR KAYA <kayapinar4706@gmail.com>
2021-04-16 18:37:01 +0300

    Ders Sonu

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -40,7 +40,7 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService, ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
-            
+            services.AddCors();
 
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
@@ -70,6 +70,7 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
